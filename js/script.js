dc06aacf86417361a814dffeb2f38d61e275dca3
Eko Mardiatno <ekomardiatno@gmail.com>
2018-07-21 15:15:24 +0700

    Signed-off-by: Eko Mardiatno <ekomardiatno@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
   $('.icon-name').each(function(){
     var a = $(this), b = a.attr('class'), c = a.text(), d = b.replace('icon-name ','');
-    a.before('<div class="col-xs-6 col-sm-3 col-md-2 col-icon">'+
+    a.before('<div class="col-xs-4 col-sm-3 col-md-2 wrap-icon">'+
+             '<div class="col-icon">'+
              '<div class="icon"><i class="'+d+' fa-'+c+'"></i></div>'+
-             '<div class="'+b+'">'+d+' fa-'+c+'</div>'+
+             '<div class="icon-name">'+d+' fa-'+c+'</div>'+
              '<input type="text" class="copy-icon" value="'+d+' fa-'+c+'"/>'+
+             '</div>'+
              '</div>');
     a.remove();
-  })
+  });
+  
   $('.col-icon').on('click',function(){
     var a = $(this), b = a.find('.copy-icon');
     b.select();
     document.execCommand("copy");
-  })
+  });
+  
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $(".col-icon").filter(function() {
+    $(".wrap-icon").filter(function() {
       $(this).toggle($(this).find('.icon-name').text().toLowerCase().indexOf(value) > -1)
     });
   });
+  
   /*alert($('.icon-name').length)*/
 })
